eb1401a59f858e4d79a28e9aef022c5297509135
Artur Piasecki <apiasecki93@gmail.com>
2023-01-14 11:00:28 +0100

    changed server response to https://artweb-openai.onrender.com

--- client/script.js
+++ client/script.js
@@ -78,7 +78,8 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server => bot's response
-  const response = await fetch('http://localhost:5000', {
+  // const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://artweb-openai.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
